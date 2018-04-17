3605a2c0bde1fe3b2cd24f3efb51de93436036da
Joe Foley <jwfoley@stanford.edu>
2018-04-17 13:23:40 -0700

    remove y as it is split by function

--- ggplot.R
+++ ggplot.R
@@ -2,7 +2,6 @@ library(ggplot2)
 
 plot.electropherogram <- function(results, # returns a ggplot object, which can be extended by adding more features
 	x = "length",
-	y = "fluorescence",
 	scales = "free_y", # scaling rules for the facets, passed to facet_wrap()
 	geom = geom_line,
 	include.ladder = FALSE
@@ -10,7 +9,7 @@ plot.electropherogram <- function(results, # returns a ggplot object, which can 
 	if (! include.ladder) results <- subset(results, name != "Ladder")
 
 	this.plot <- ggplot(results) +
-		aes_string(x, y) +
+		aes_(as.name(x), ~fluorescence) +
 		geom() +
 		facet_wrap(~ well.number, scales = scales)
 	
@@ -20,7 +19,6 @@ plot.electropherogram <- function(results, # returns a ggplot object, which can 
 	if (x == "length") this.plot <- this.plot + xlab("length (bases)")
 	if (x == "time") this.plot <- this.plot + xlab("time (s)")
 	if (x == "distance") this.plot <- this.plot + xlab("distance migrated") + scale_x_reverse()
-	if (y == "delta.molarity") this.plot <- this.plot + ylab("concentration (pM)")
 		
 	this.plot
 }
