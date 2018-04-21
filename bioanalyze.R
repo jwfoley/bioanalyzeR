134ae737b9e31b43ea88fe44fa5a09a5a149ef5a
Joe Foley <jwfoley@stanford.edu>
2018-04-20 18:15:11 -0700

    rework length calculation but still need molarity

--- bioanalyze.R
+++ bioanalyze.R
@@ -1,25 +1,24 @@
 library(XML)
 library(openssl)
 
-read.bioanalyzer <- function(xml.files, fit = "linear") {
-	stopifnot(fit %in% c("linear", "spline"))
+read.bioanalyzer <- function(xml.files, fit = "spline") {
+	stopifnot(fit %in% c("linear", "spline", "regression"))
 
-	combined.results <- do.call(rbind, lapply(xml.files, function(xml.file) {
+	results <- do.call(rbind, lapply(xml.files, function(xml.file) {
 	
 		xml.root <- xmlRoot(xmlParse(xml.file))
 		
-		# extract ladder specifications
-		ladder <- data.frame(t(xmlSApply(xml.root[["Chips"]][["Chip"]][["AssayBody"]][["DAAssaySetpoints"]][["DAMAssayInfoMolecular"]][["LadderPeaks"]], function(standard) c(as.numeric(xmlValue(standard[["Size"]])), as.numeric(xmlValue(standard[["Concentration"]]))))), row.names = NULL)
-		names(ladder) <- c("length", "concentration")
-		ladder$molarity <- ladder$concentration / ladder$length / 0.00066 # magic number computed from Agilent standard curves
-
-		# extract samples
+		assay.definitions <- xml.root[["Chips"]][["Chip"]][["AssayBody"]][["DAAssaySetpoints"]][["DAMAssayInfoMolecular"]]
+		ladder.definition <- data.frame(t(xmlSApply(assay.definitions[["LadderPeaks"]], function(standard) sapply(c("Size", "Concentration"), function (field) as.numeric(xmlValue(standard[[field]]))))), row.names = NULL)
+		marker.aligned.times <- sapply(c("VirtualLowerMarkerTime", "VirtualUpperMarkerTime"), function (field) as.numeric(xmlValue(assay.definitions[[field]])))
+		
+		# read raw sample data
 		samples <- xml.root[["Chips"]][["Chip"]][["Files"]][["File"]][["Samples"]]
-		results <- do.call(rbind, xmlApply(samples, function(this.sample) {
-			if (xmlValue(this.sample[["HasData"]]) == "false") NULL else {
+		result <- do.call(rbind, xmlApply(samples, function(this.sample) {
+			if (xmlValue(this.sample[["HasData"]]) != "true") NULL else {
 				signal.data <- this.sample[["DASignals"]][["DetectorChannels"]][[1]][["SignalData"]]
 				n.values <- as.integer(xmlValue(signal.data[["NumberOfSamples"]]))
-				result <- data.frame(
+				raw.data <- data.frame(
 					well.number = as.integer(xmlValue(this.sample[["Index"]])),
 					name = xmlValue(this.sample[["Name"]]),
 					time = as.numeric(xmlValue(signal.data[["XStart"]])) + as.numeric(xmlValue(signal.data[["XStep"]])) * (1:n.values - 1),
@@ -27,37 +26,77 @@ read.bioanalyzer <- function(xml.files, fit = "linear") {
 					stringsAsFactors = F
 				)
 				
-				# fit standard curve for molecule length (done once per sample because that's how the results are provided, even though it should be the same standard curve for every sample)
-				ladder.peaks <- this.sample[["DAResultStructures"]][["DARStandardCurve"]][[1]][["LadderPeaks"]]
-				stopifnot(all.equal(xmlSApply(ladder.peaks, function(peak) as.numeric(xmlValue(peak[["FragmentSize"]]))), ladder$length, check.names = F)) # verify it matches the known ladder
-				this.ladder <- cbind(ladder, data.frame(t(xmlSApply(ladder.peaks, function(peak) c(as.numeric(xmlValue(peak[["MigrationTime"]])), as.numeric(xmlValue(peak[["Area"]]))))), row.names = NULL))
-				names(this.ladder)[ncol(ladder) + 1:2] <- c("time", "area")
+				# align the observation times according to the markers in this sample
+				peaks.sample <- this.sample[["DAResultStructures"]][["DARIntegrator"]][["Channel"]][["PeaksMolecular"]]
+				which.lower.marker <- which(xmlSApply(peaks.sample, function(peak) xmlValue(peak[["Observations"]]) == "Lower Marker"))
+				stopifnot(length(which.lower.marker) == 1)
+				stopifnot(as.numeric(xmlValue(peaks.sample[[which.lower.marker]][["AlignedMigrationTime"]])) == marker.aligned.times[1])
+				which.upper.marker <- which(xmlSApply(peaks.sample, function(peak) xmlValue(peak[["Observations"]]) == "Upper Marker"))
+				stopifnot(length(which.upper.marker) == 1)
+				stopifnot(as.numeric(xmlValue(peaks.sample[[which.upper.marker]][["AlignedMigrationTime"]])) == marker.aligned.times[2])
+				alignment.coefficient <- diff(marker.aligned.times) / diff(sapply(c(which.lower.marker, which.upper.marker), function(i) as.numeric(xmlValue(peaks.sample[[i]][["MigrationTime"]]))))
+				alignment.offset <- marker.aligned.times[1] - alignment.coefficient * as.numeric(xmlValue(peaks.sample[[which.lower.marker]][["MigrationTime"]]))
+				raw.data$aligned.time <- raw.data$time * alignment.coefficient + alignment.offset
 				
-				if (fit == "linear") {
-					standard.curve.function <- approxfun(this.ladder$time, this.ladder$length)		
-				} else if (fit == "spline") {
-					standard.curve.function <- splinefun(this.ladder$time, this.ladder$length, method = "natural")
-				}
-				result$length <- standard.curve.function(result$time)
-				
-				# convert to molarity
-				this.ladder$normalized.fluorescence <- this.ladder$area / this.ladder$length / this.ladder$time
-				fluorescence.coefficient <- lm(molarity ~ normalized.fluorescence - 1, this.ladder)$coefficients
-				length.derivative <- diff(result$length) / diff(result$time)
-				result$molarity <- result$fluorescence * fluorescence.coefficient / result$time / c(NA, length.derivative) / result$length
-			
-				result
+				raw.data
 			}
 		}))
 		
-		cbind(batch = sub("\\.xml$", "", basename(xml.file)), results)
+		# analyze ladder
+		which.ladder <- which(xmlApply(samples, function(this.sample) xmlValue(this.sample[["Category"]])) == "Ladder")
+		stopifnot(length(which.ladder) == 1)
+		sample.ladder <- samples[[which.ladder]]
+		stopifnot(xmlValue(sample.ladder[["HasData"]]) == "true")
+		peaks.ladder <- data.frame(t(xmlSApply(sample.ladder[["DAResultStructures"]][["DARIntegrator"]][["Channel"]][["PeaksMolecular"]], function(peak) sapply(c("AlignedMigrationTime", "Area", "FragmentSize", "Molarity", "Concentration", "AlignedStartTime", "AlignedEndTime"), function(field) as.numeric(xmlValue(peak[[field]]))))), row.names = NULL)
+		stopifnot(all.equal(ladder.definition, peaks.ladder[,c("FragmentSize", "Concentration")], check.names = F))
+		
+		# fit standard curve for molecule length
+		# do this with aligned times so it's effectively recalibrated for each sample's markers
+		if (fit == "interpolate") {
+			warning("linear interpolation gives ugly results for molarity estimation")
+			standard.curve.function <- approxfun(peaks.ladder$AlignedMigrationTime, peaks.ladder$FragmentSize)
+		} else if (fit == "spline") {
+			standard.curve.function <- splinefun(peaks.ladder$AlignedMigrationTime, peaks.ladder$FragmentSize, method = "natural")
+		} else if (fit == "regression") {
+			mobility.model <- lm(1/AlignedMigrationTime ~ log(FragmentSize), data = peaks.ladder)
+			standard.curve.function <- function(time) exp((1 / time - mobility.model$coefficients[1]) / mobility.model$coefficients[2])
+		}
+		result$length <- sapply(result$aligned.time, function(x) if (x < marker.aligned.times[1] || x > marker.aligned.times[2]) NA else  standard.curve.function(x)) # avoid extrapolating
+		
+#		# convert to molarity
+#		
+#	
+#				# fit standard curve for molecule length (done once per sample because that's how the results are provided, even though it should be the same standard curve for every sample)
+#				ladder.peaks <- this.sample[["DAResultStructures"]][["DARStandardCurve"]][[1]][["LadderPeaks"]]
+#				stopifnot(all.equal(xmlSApply(ladder.peaks, function(peak) as.numeric(xmlValue(peak[["FragmentSize"]]))), ladder$length, check.names = F)) # verify it matches the known ladder
+#				this.ladder <- cbind(ladder, data.frame(t(xmlSApply(ladder.peaks, function(peak) c(as.numeric(xmlValue(peak[["MigrationTime"]])), as.numeric(xmlValue(peak[["Area"]]))))), row.names = NULL))
+#				names(this.ladder)[ncol(ladder) + 1:2] <- c("time", "area")
+#				
+#				if (fit == "linear") {
+#					standard.curve.function <- approxfun(this.ladder$time, this.ladder$length)		
+#				} else if (fit == "spline") {
+#					standard.curve.function <- splinefun(this.ladder$time, this.ladder$length, method = "natural")
+#				}
+#				result$length <- standard.curve.function(result$time)
+#				
+#				# convert to molarity
+#				this.ladder$normalized.fluorescence <- this.ladder$area / this.ladder$length / this.ladder$time
+#				fluorescence.coefficient <- lm(molarity ~ normalized.fluorescence - 1, this.ladder)$coefficients
+#				length.derivative <- diff(result$length) / diff(result$time)
+#				result$molarity <- result$fluorescence * fluorescence.coefficient / result$time / c(NA, length.derivative) / result$length
+#			
+#				result
+#			}
+#		}))
+		
+		cbind(batch = sub("\\.xml$", "", basename(xml.file)), result)
 	}))
 	
 	# format output nicely
-	rownames(combined.results) <- NULL
-	combined.results$batch <- factor(combined.results$batch, levels = unique(combined.results$batch)) # make batches into a factor that keeps them in the observed order
-	combined.results$name <- factor(combined.results$name, levels = unique(combined.results$name)) # make names into a factor that keeps them in the observed order
+	rownames(results) <- NULL
+	results$batch <- factor(results$batch, levels = unique(results$batch)) # make batches into a factor that keeps them in the observed order
+	results$name <- factor(results$name, levels = unique(results$name)) # make names into a factor that keeps them in the observed order
 	
-	combined.results
+	results
 }
 
