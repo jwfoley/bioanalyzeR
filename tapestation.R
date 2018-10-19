fb3d3085ae58b97c213f0d4c7a2ac68f6c000dee
Joe Foley <jwfoley@stanford.edu>
2018-10-18 20:22:15 -0700

    suppress warnings from missing XML values

--- tapestation.R
+++ tapestation.R
@@ -82,15 +82,17 @@ read.tapestation.xml <- function(xml.files) {
 			
 			reagent.id <- xmlValue(sample.xml[["ScreenTapeID"]])
 			
-			peaks <- do.call(rbind, xmlApply(sample.xml[["Peaks"]], function(peak.xml) data.frame(
-				peak.observations =  trimws(xmlValue(peak.xml[["Observations"]])),
-				length =             as.integer(xmlValue(peak.xml[["Size"]])),
-				distance =           as.numeric(xmlValue(peak.xml[["RunDistance"]])) / 100,
-				lower.bound =        as.numeric(xmlValue(peak.xml[["FromPercent"]])) / 100,
-				upper.bound =        as.numeric(xmlValue(peak.xml[["ToPercent"]])) / 100, 
-				area =               as.numeric(xmlValue(peak.xml[["Area"]])),
-				molarity =           as.numeric(xmlValue(peak.xml[["Molarity"]])
-			))))
+			suppressWarnings( # will throw warnings if missing values are coerced to NA but we can live with that
+				peaks <- do.call(rbind, xmlApply(sample.xml[["Peaks"]], function(peak.xml) data.frame(
+					peak.observations =  trimws(xmlValue(peak.xml[["Observations"]])),
+					length =             as.integer(xmlValue(peak.xml[["Size"]])),
+					distance =           as.numeric(xmlValue(peak.xml[["RunDistance"]])) / 100,
+					lower.bound =        as.numeric(xmlValue(peak.xml[["FromPercent"]])) / 100,
+					upper.bound =        as.numeric(xmlValue(peak.xml[["ToPercent"]])) / 100, 
+					area =               as.numeric(xmlValue(peak.xml[["Area"]])),
+					molarity =           as.numeric(xmlValue(peak.xml[["Molarity"]])
+				))))
+			)
 			peaks$relative.distance <- (peaks$distance - min(peaks$distance)) / (max(peaks$distance) - min(peaks$distance))
 			
 			cbind(well.number, well.row, well.col, name, reagent.id, sample.observations, peaks)
