053d9b1f1e5872b4ddc1f8cf3ef7226cf6e1d565
Joe Foley <jwfoley@stanford.edu>
2019-05-29 12:13:49 -0700

    fix typo

--- tapestation.R
+++ tapestation.R
@@ -116,7 +116,7 @@ read.tapestation <- function(xml.file, gel.image.file = NULL, fit = "spline") {
 	result <- read.tapestation.gel.image(gel.image.file)
 	stopifnot(length(unique(result$gel.lane)) == length(unique(peaks$well.number)))
 	
-	# get sample names andand observations (might not be unique but we assume well numbers are)
+	# get sample names and observations (might not be unique but we assume well numbers are)
 	sample.table <- unique(peaks[,c("batch", "reagent.id", "name", "sample.observations", "well.number", "well.row", "well.col")])
 	stopifnot(nrow(sample.table) == length(unique(result$gel.lane)))
 	
