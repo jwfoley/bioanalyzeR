a790d05c48e080175d4ea388157020e96d070168
Joe Foley <jwfoley@stanford.edu>
2020-03-18 12:29:26 -0700

    add the in. functions

--- R/electrophoresis.R
+++ R/electrophoresis.R
@@ -27,3 +27,106 @@ rbind.electrophoresis <- function(...) {
 	), class = "electrophoresis")
 }
 
+#' Check whether data points are within a custom region
+#'
+#' This function takes the \code{$data} element of an \code{electrophoresis} object and the boundaries of a region, for a desired variable, and checks whether each data point is within that region.
+#'
+#' @param data A data frame of electrophoresis data, from the \code{$data} member of an \code{electrophoresis} object (not the whole object itself).
+#' @param lower.bound Lower boundary of the region to integrate, or \code{NULL} to extend to negative infinity.
+#' @param upper.bound Upper boundary of the region to integrate, or \code{NULL} to extend to positive infinity.
+#' @param bound.variable Which variable the boundaries refer to, e.g. \code{"length"}.
+#'
+#' @return A vector of logicals with length \code{nrow(data)}.
+#'
+#' @seealso \code{\link{in.region}}, \code{\link{in.peak}}
+#'
+#' @export
+in.custom.region <- function(
+	data,
+	lower.bound = -Inf,
+	upper.bound = Inf,
+	bound.variable = "length"
+) data[[bound.variable]] >= lower.bound & data[[bound.variable]] <= upper.bound
+
+#' Check whether data points are within a reported peak
+#'
+#' This function takes an electrophoresis object and checks whether each data point is within a specified peak.
+#'
+#' Because each peak in the table belongs to a specific sample, only data points from that sample are \code{TRUE}.
+#'
+#' @param electrophoresis An \code{electrophoresis} object.
+#' @param which.peak An integer indicating one of the peaks in \code{electrophoresis$peaks}.
+#'
+#' @return A vector of logicals with length \code{nrow(data)}.
+#'
+#' @seealso \code{\link{in.region}}, \code{\link{in.custom.region}}, \code{\link{in.peaks}}
+#'
+#' @export
+in.peak <- function(electrophoresis, which.peak) {
+	electrophoresis$data$batch == electrophoresis$peaks$batch[which.peak] &
+	electrophoresis$data$well.number == electrophoresis$peaks$well.number[which.peak] &
+	! is.na(electrophoresis$data$length) &
+	electrophoresis$data$length >= electrophoresis$peaks$lower.length[which.peak] &
+	electrophoresis$data$length <= electrophoresis$peaks$upper.length[which.peak]
+}
+
+#' Check whether data points are within a reported region
+#'
+#' This function takes an electrophoresis object and checks whether each data point is within a specified region.
+#'
+#' Because each region in the table belongs to a specific sample, only data points from that sample are \code{TRUE}.
+#'
+#' @param electrophoresis An \code{electrophoresis} object.
+#' @param which.region An integer indicating one of the region in \code{electrophoresis$regions}.
+#'
+#' @return A vector of logicals with length \code{nrow(data)}.
+#'
+#' @seealso \code{\link{in.peak}}, \code{\link{in.custom.region}}, \code{\link{in.regions}}
+#'
+#' @export
+in.region <- function(electrophoresis, which.region) {
+	electrophoresis$data$batch == electrophoresis$regions$batch[which.region] &
+	electrophoresis$data$well.number == electrophoresis$regions$well.number[which.region] &
+	! is.na(electrophoresis$data$length) &
+	electrophoresis$data$length >= electrophoresis$regions$lower.length[which.region] &
+	electrophoresis$data$length <= electrophoresis$regions$upper.length[which.region]
+}
+
+#' Match data points to the peaks they are in
+#'
+#' This function takes an electrophoresis object and reports which of the peaks each data point belongs to, if any.
+#'
+#' Warning: If peaks in the reported table overlap, any data point in more than one peak will only be matched to the last peak it belongs to.
+#'
+#' @param electrophoresis An \code{electrophoresis} object.
+#'
+#' @return A vector of integers with length \code{nrow(data)}. Each element is either an integer indicating the row of \code{electrophoresis$peaks} that the data point belongs to, or NA if it is not in any of the annotated peaks.
+#'
+#' @seealso \code{\link{in.regions}}, \code{\link{in.peak}}
+#'
+#' @export
+in.peaks <- function(electrophoresis) {
+	result <- rep(NA, nrow(electrophoresis$data))
+	if (! is.null(electrophoresis$peaks)) for (i in 1:nrow(electrophoresis$peaks)) result[which(in.peak(electrophoresis, i))] <- i
+	result
+}
+
+#' Match data points to the regions they are in
+#'
+#' This function takes an electrophoresis object and reports which of the regions each data point belongs to, if any.
+#'
+#' Warning: If regions in the reported table overlap, any data point in more than one region will only be matched to the last region it belongs to.
+#'
+#' @param electrophoresis An \code{electrophoresis} object.
+#'
+#' @return A vector of integers with length \code{nrow(data)}. Each element is either an integer indicating the row of \code{electrophoresis$regions} that the data point belongs to, or NA if it is not in any of the annotated regions.
+#'
+#' @seealso \code{\link{in.peaks}}, \code{\link{in.region}}
+#'
+#' @export
+in.regions <- function(electrophoresis) {
+	result <- rep(NA, nrow(electrophoresis$data))
+	if (! is.null(electrophoresis$regions)) for (i in 1:nrow(electrophoresis$regions)) result[which(in.region(electrophoresis, i))] <- i
+	result
+}
+
